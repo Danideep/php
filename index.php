ca4c0624c3b8010b69506cc1ff58c29f50037704
Danideep <90630389+Danideep@users.noreply.github.com>
2022-02-03 14:06:14 +0330

    202
    202

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1); // strict requirement ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcoe to php tutorial</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+ 
+    <button type="button" class="btn btn-outline-success" id="reset">Success</button>
+    <div class="container">
+           <h1 >Hello  php</h1>
+
+<?php
+echo "hi";
+?>
+     </div>
+
+</body>
+<script src="script.js"></script>
+</html>
